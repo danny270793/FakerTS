67f2ea73647a88a2a97add671b8c868056b5ec2d
Danny Vaca <danny270793@icloud.com>
2025-10-19 14:33:26 -0500

    format code

--- src/libraries/base.ts
+++ src/libraries/base.ts
@@ -1,5 +1,5 @@
 export default class Base {
-    public static numberBetween(min: number, max: number): number {
-        return Math.floor(Math.random() * (max - min + 1)) + min
-    }
+  public static numberBetween(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 }
