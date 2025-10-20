478f3224d08f64b408c515320383fdeacf7c0bb3
Danny Vaca <danny270793@icloud.com>
2025-10-20 00:16:29 -0500

    Enhance Arrays utility class: rename random method to randomItem, add randomItems and shuffle methods for improved array manipulation.

--- src/libraries/arrays.ts
+++ src/libraries/arrays.ts
@@ -1,7 +1,16 @@
 import Numbers from "./numbers";
 
 export default class Arrays {
-  public static random<T>(array: T[]): T {
+  public static randomItem<T>(array: T[]): T {
     return array[Numbers.between(0, array.length - 1)] as T;
+  }
+  public static randomItems<T>(array: T[], quantity: number): T[] {
+    return Arrays.shuffle(array).slice(0, quantity);
+  }
+  public static random(quantity: number): number[] {
+    return Array.from({ length: quantity }, () => Numbers.digit());
+  }
+  public static shuffle<T>(array: T[]): T[] {
+    return array.sort(() => Numbers.between(0, 1) - 0.5);
   }
 }
